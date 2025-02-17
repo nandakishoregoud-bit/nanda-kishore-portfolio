c71b14a74e066f59adfb2233e8408a7e570bd916
nandakishoregoud-bit <yvvnandak@gmail.com>
2025-02-17 10:12:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Navigation Active Link Highlight
-    let navLinks = document.querySelectorAll(".nav-link");
+    let navLinks = document.querySelectorAll(".nav-manu");
     navLinks.forEach(link => {
         if (link.getAttribute("href") === currentPath) {
             link.classList.add("active");
